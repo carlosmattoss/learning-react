5bb5df0bc91d9c926ee1a34cbb4dd31b9c8a180d
Carlos Matos <carlos_mattoos@hotmail.com>
2024-01-05 17:07:00 -0300

    feat: rendering lists

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,22 @@
 function ListGroup() {
+  let items = [
+    'New York',
+    'London',
+    'Sao Paulo',
+    'Paris',
+    'Tokyo'
+  ];
+
+  items = [];
+
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </>
   );
